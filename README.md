5da6c367ae11999d6e39e4cd9f073864120af637
Kazumasa Utashiro <kaz@utashiro.com>
2025-01-19 17:32:23 +0900

    minil new Getopt::EX::Config

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Getopt::EX::Config - It's new $module
+
+# SYNOPSIS
+
+    use Getopt::EX::Config;
+
+# DESCRIPTION
+
+Getopt::EX::Config is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/Getopt/EX/Config.pm
@@ -0,0 +1,39 @@
+package Getopt::EX::Config;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Getopt::EX::Config - It's new $module
+
+=head1 SYNOPSIS
+
+    use Getopt::EX::Config;
+
+=head1 DESCRIPTION
+
+Getopt::EX::Config is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
